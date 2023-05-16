8b1efa7938ad89f9508920ff4cf9b3b30776ec39
gnwks <121499392+not-so-lonely@users.noreply.github.com>
2023-05-16 20:48:36 +0500

    tanchiki.py

--- tanchiki.py
+++ tanchiki.py
@@ -1,7 +1,6 @@
 from pygame import*
-import random
-#что еще делать: рандомное появление врагов в верхней полоске
-#стенки!!!!!!!!!
+from random import randint
+#что еще делать: враги
 #спрайт коллайд рект
 
 clock = time.Clock()#часы для игры
@@ -28,27 +27,41 @@ lvl1 = transform.scale(lvl1,(700,600))
 ###################################################################################
 
 
+class stena(sprite.Sprite):
+    def __init__(self,  x, y,width,height):
+        super().__init__()
+        self.image = Surface((width,height))
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+    def ris(self):
+        okno.blit(self.image, (self.rect.x , self.rect.y )) 
 class sprait(sprite.Sprite):
     def __init__(self, kartinka, x, y, napr):
         super().__init__()
-        self.image = transform.scale(image.load(kartinka), (72, 40))
+        self.image = transform.scale(image.load(kartinka), (40, 40))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.napr = napr #задаем типо направление
+        self.lasx = x
+        self.lasy = y
 
     def ris(self):
         if self.napr=="up":
             okno.blit(self.image, (self.rect.x, self.rect.y))
         elif self.napr =="left":
             self.newimage = transform.rotate(self.image, 90) #делаем новую картинку через старую + что то
-            okno.blit(self.newimage, (self.rect.x + 20, self.rect.y - 4)) #вставляем новую картнику
+            okno.blit(self.newimage, (self.rect.x, self.rect.y)) #вставляем новую картнику
         elif self.napr =="down":
             self.newimage = transform.rotate(self.image, 180) #делаем новую картинку через старую + что то
             okno.blit(self.newimage, (self.rect.x, self.rect.y)) #вставляем новую картнику
         elif self.napr =="right":
             self.newimage = transform.rotate(self.image, 270) #делаем новую картинку через старую + что то
-            okno.blit(self.newimage, (self.rect.x + 20, self.rect.y - 4)) #вставляем новую картнику
+            okno.blit(self.newimage, (self.rect.x, self.rect.y)) #вставляем новую картнику
 ###################################################################################
 ###################################################################################
 
@@ -58,16 +71,26 @@ class igrok(sprait):
         knopki = key.get_pressed()
         if knopki[K_RIGHT] and self.rect.x<630:
             self.napr = "right"
+            self.lasx=self.rect.x
+            self.lasy=self.rect.y
             self.rect.x += 5
         if knopki[K_LEFT] and self.rect.x>-20:
             self.napr = "left"
+            self.lasx=self.rect.x
+            self.lasy=self.rect.y
             self.rect.x -= 5
         if knopki[K_UP] and self.rect.y>-5:
             self.napr = "up"
+            self.lasx=self.rect.x
+            self.lasy=self.rect.y
             self.rect.y -= 5
         if knopki[K_DOWN] and self.rect.y<550:
             self.napr = "down"
+            self.lasx=self.rect.x
+            self.lasy=self.rect.y
             self.rect.y += 5
+
+
 
 ###################################################################################
 ###################################################################################
@@ -96,21 +119,31 @@ class bullet(sprait):
         if self.napr == "right":
             self.rect.x += 7
 
-
-
+lifes = 3
 
 class vrag(sprait):
     def __init__(self, kartinka, x, y,napr):
         super().__init__(kartinka, x, y, napr)
-        self.image = transform.scale(image.load(kartinka), (72, 40)) 
+        self.image = transform.scale(image.load(kartinka), (40, 40)) 
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def ezda(self,x1,x2):
         self.ris()
-        self.rect.x = randint(0,632)
-        self.rect.y = 0
-    def collide(self,p):
+        if self.rect.x < x1:
+            self.napr = "right"
+        if self.rect.x > x2:
+            self.napr = "left"
+        if self.napr == "left":
+            self.rect.x -= 5   
+        else:
+            self.rect.x += 5
+    def collide(self, p):
         if sprite.collide_rect(self,p):
-            pass
-            
-
+            tanksus.rect.x = 300
+            tanksus.rect.y = 500
+            global lifes
+            lifes -= 1
 
 
 
@@ -129,6 +162,15 @@ class vrag(sprait):
 tanksus = igrok('tank_gg.png',300,210, "up")
 
 puli = list()
+
+
+tankvrag = vrag('tank_gg2.png',650,210,"left")
+
+walls = [
+    stena(0, 0, 60, 600),
+    stena(640, 0, 60, 600)
+]
+
 
 ###################################################################################
 ###################################################################################
@@ -178,21 +220,27 @@ while game:
                 b1 = bullet('bullet.png', tanksus.rect.x + 36, tanksus.rect.y + 20, tanksus.napr)
                 puli.append(b1)        
 
-    okno.blit(lvl1,(0,0))   
+
     
+
     if i.type == MOUSEBUTTONDOWN:
         if i.button == 1:
             print(i.pos)
+    okno.blit(lvl1,(0,0))   
 
+    for w in walls:
+        w.ris()
+        if sprite.collide_rect(w, tanksus):
+            tanksus.rect.x = tanksus.lasx
+            tanksus.rect.y = tanksus.lasy
     for b in puli:
         b.vistrel()
         if b.rect.x < 0:
             puli.remove(b)
     
     
-
-
-
+    
+    tankvrag.ezda(60,600)
 
     tanksus.upravl()
 
